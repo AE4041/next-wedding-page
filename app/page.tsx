d248a51b0bcd1b0fdf61d108a33da85b70ab977e
Emmanuel <140660538+AE4041@users.noreply.github.com>
2026-05-25 01:39:53 +0000

    added hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,69 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useRef, useState, useEffect } from "react";
+import { Mea_Culpa } from 'next/font/google';
+
+const meaCulpa = Mea_Culpa({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+
+export default function AudioPlayer() {
+  const audioRef = useRef(null);
+  const videoRef = useRef(null)
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  
+  const playAudio = () => {
+    if (!isPlaying) {
+      audioRef.current.play();
+      videoRef.current.play()
+      setIsPlaying(true);
+    }
+  };
+
+  const stopAudio = (e) => {
+    e.stopPropagation(); // prevent page click from retriggering play
+    audioRef.current.pause();
+    audioRef.current.currentTime = 0;
+    setIsPlaying(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", playAudio);
+    return () => document.removeEventListener("click", playAudio);
+  }, [isPlaying]); // re-register when isPlaying changes
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="relative min-h-screen flex flex-col items-center justify-center py-8 px-6 overflow-hidden bg-background">
+      <audio ref={audioRef} src="/audio/song.mp3" loop preload="auto" />
+
+      <div className="absolute inset-0 w-full h-full">
+        <video ref={videoRef} className="absolute inset-0 w-full h-full object-cover" src="/video/video4.mp4" loop playsInline preload="auto"></video>
+      </div>
+
+      <div className="text-center z-10 animate-fade-in my-57.5 bg-gray-90 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-50 p-10">
+        <p className="text-xs md:text-sm tracking-[0.3em] uppercase mb-3 text-foreground">
+          We are getting married
+        </p>
+        <h1 className={`${meaCulpa.className} text-5xl md:text-6xl lg:text-8xl mb-2 text-amber-950`}>Evans & Sparcil</h1>
+        <div className="flex items-center justify-center gap-4 md:my-8 my-1.25">
+          <span className="h-px w-12 md:w-20 bg-amber-950"></span>
+          <span className="text-amber-500 text-lg drop-shadow-md">✦</span>
+          <span className="h-px w-12 md:w-20 bg-amber-950"></span>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+
+        <p className="text-xs md:text-sm tracking-[0.3em] uppercase mb-3 text-foreground">27th June, 2026</p>
+      </div>
+
+      {isPlaying && (
+        <button
+          className="fixed bottom-6 right-6 z-50 w-12 h-12 rounded-full border border-foreground bg-white backdrop-blur-sm flex items-center justify-center text-foreground hover:text-foreground hover:border-foreground/50 transition-all duration-300"
+          onClick={stopAudio}
+        ></button>
+      )}
     </div>
   );
 }
