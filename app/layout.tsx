7d4a9371243af1207720deba8ae5c283e4305543
Emmanuel <140660538+AE4041@users.noreply.github.com>
2026-06-07 13:22:27 +0000

    Updated meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,26 +2,39 @@ import type { Metadata } from "next";
 import { Cabin } from "next/font/google";
 import "./globals.css";
 
-const cabinFont = Cabin({ subsets: ["latin"], variable: "--font-cabin" });
-
+const cabinFont = Cabin({
+  subsets: ["latin"],
+  variable: "--font-cabin",
+});
 
 export const metadata: Metadata = {
-  title: "Digital Invite - Evans & Sparcil",
-  description: "We are getting married.",
+  metadataBase: new URL("https://evans-sparcil.vercel.app"),
+  title: "Evans ❤️ Sparcil | Wedding Invitation",
+  description: "We joyfully invite you to celebrate our wedding and share in our special day.",
   openGraph: {
-    title: "Digital Invite - Evans & Sparcil",
-    description: "You are invited to our Wedding",
-    url: "https://evans-sparcil.vercel.app/",
+    title: "Evans ❤️ Sparcil | Wedding Invitation",
+    description:"We joyfully invite you to celebrate our wedding and share in our special day.",
+    url: "https://evans-sparcil.vercel.app",
     siteName: "Evans & Sparcil",
     images: [
       {
-        url: "/images/pic1.jpg",
+        url: "https://evans-sparcil.vercel.app/images/pic1.jpg",
         width: 1200,
-        height: 600,
-        alt: "Digital Invitation",
+        height: 630,
+        alt: "Wedding Invitation",
       },
-    ]
-  }
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Evans ❤️ Sparcil | Wedding Invitation",
+    description:
+      "We joyfully invite you to celebrate our wedding and share in our special day.",
+    images: ["/images/pic4.jpg"],
+  },
 };
 
 export default function RootLayout({
@@ -31,7 +44,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${cabinFont.variable} antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+      </body>
     </html>
   );
 }
